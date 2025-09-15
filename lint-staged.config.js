5bccd5ffd7f3cab4339ba9cf5e89aa2d242c2b77
shaibuuneks <shaibuuneks@gmail.com>
2025-09-15 10:07:00 +0000

    CodeQL and Gitleaks

---
+++ lint-staged.config.js
@@ -0,0 +1,7 @@
+// lint-staged.config.js
+module.exports = {
+    "src/**/*.{js,jsx}": ["eslint --fix", "prettier --write"],
+    "tests/**/*.{js,jsx}": ["eslint --fix", "prettier --write"],
+    "*.json": ["prettier --write"]
+  };
+  
