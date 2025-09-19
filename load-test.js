019d3dc917d897121d97805b64daddc9bc1fcd25
shaibuuneks <shaibuuneks@gmail.com>
2025-09-19 14:30:35 +0000

    alert rules

--- load-test.js
+++ load-test.js
@@ -2,15 +2,18 @@ const axios = require("axios");
 
 const urls = [
   "https://mydev-staging.onrender.com/",
-  "https://mydev-staging.onrender.com/debug-sentry"
+  "https://mydev-staging.onrender.com/debug-sentry",
 ];
 
 async function sendRequests() {
   for (let i = 0; i < 100; i++) {
     for (const url of urls) {
-      axios.get(url)
-        .then(res => console.log(` ${url} ${res.status}`))
-        .catch(err => console.log(` ${url} ${err.response?.status || err.message}`));
+      axios
+        .get(url)
+        .then((res) => console.log(` ${url} ${res.status}`))
+        .catch((err) =>
+          console.log(` ${url} ${err.response?.status || err.message}`)
+        );
     }
   }
 }
